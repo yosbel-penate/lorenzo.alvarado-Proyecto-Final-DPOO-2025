903b2388168b76491ecff0472da44d93901666af
johanmoransarmiento <johanmoransarmiento@gmail.com>
2025-04-02 15:16:55 -0400

    add Objet Magic

---
+++ Galactic Oddissey/src/main/java/org/inf/galacticoddissey/entities/MagicObjet/PocionCurativa.java
@@ -0,0 +1,18 @@
+public class PocionCurativa extends ObjetoMagico {
+
+        public PocionCurativa(String nombre, String efecto, int rangoUso) {
+            super(nombre, efecto, rangoUso);
+        }
+
+        @Override
+        public void usar() {
+            // Implementación
+        }
+
+        public void restauraVida() {
+            // Habilidad especial
+        }
+    }
+
+
+}
