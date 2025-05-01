df5bcce0954495d41a4d521939ab85e99f4bd275
Jonathanhrt <jonathan.hrt30@gmail.com>
2025-05-01 19:31:03 -0400

    Add files via upload
    Nuevo proyecto en el cual vamos a trabajar

---
+++ Galactic odyssey/src/main/java/App/App.java
@@ -0,0 +1,53 @@
+package App;
+
+import View.Maps.GameFactory;
+import View.Maps.Map_1.MapLoader;
+import View.UI.MenuController;
+import Domain.Entity.Characters.Players.Player;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
+
+import static com.almasb.fxgl.dsl.FXGL.getGameWorld;
+
+public class App extends GameApplication {
+    private MenuController menuController;
+    private MapLoader mapLoader;
+    private Player player;
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(1500);
+        settings.setHeight(800);
+        settings.setTitle("Galactic Odyssey");
+    }
+
+    @Override
+    protected void initGame() {
+        // Registrar el GameFactory para spawnear entidades (obstáculos, etc.)
+        getGameWorld().addEntityFactory(new GameFactory());
+    }
+
+    @Override
+    protected void initUI() {
+        menuController = new MenuController();
+        menuController.showMenu();
+    }
+
+    public void startNewGame() {
+        // Se limpia el mundo y la UI
+        getGameWorld().removeEntities();
+        FXGL.getGameScene().clearGameViews();
+        FXGL.getGameScene().clearUINodes();
+
+        mapLoader = new MapLoader();
+        mapLoader.loadMap();
+        // Se crea al jugador.
+        player = new Player();
+        player.spawnPlayer();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
