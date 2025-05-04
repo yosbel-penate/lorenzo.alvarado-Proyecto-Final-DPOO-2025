8d3e0410d63423a4923056f49c7c7e5caa481356
johanmoransarmiento <johanmoransarmiento@gmail.com>
2025-05-04 18:04:51 -0400

    Update EntityType.java

--- Galactic odyssey/src/main/java/Domain/Entity/EntityType.java
+++ Galactic odyssey/src/main/java/Domain/Entity/EntityType.java
@@ -1,6 +1,10 @@
 package Domain.Entity;
 
 public enum EntityType {
-    PLAYER,
-    OBSTACULOS,
+    HERO,         // Represents the hero
+    ENEMY,        // Represents the enemies
+    MAGIC_OBJECT, // Represents magical objects
+    PLAYER,       // Represents the player
+    ITEM,         // Represents an item
+    OBSTACLE      // Represents an obstacle
 }
